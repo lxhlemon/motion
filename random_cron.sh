41fdda1747bebf9fcfa2a0ce8225580cba228e46
lxhlemon <62836770+lxhlemon@users.noreply.github.com>
2022-08-26 18:23:43 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -4,21 +4,21 @@
 #     time=$(($RANDOM%10))" 2 * * *"
 # elif [[ $(date '+%H') == 02 ]];then
 #     time=$(($RANDOM%10))" 5 * * *"
-# elif [[ $(date '+%H') == 05 ]];then
+# elif [[ $(date '+%H') == 04 ]];then
 #     time=$(($RANDOM%10))" 7 * * *"
-# elif [[ $(date '+%H') == 07 ]];then
+# elif [[ $(date '+%H') == 06 ]];then
 #     time=$(($RANDOM%10))" 9 * * *"
-# elif [[ $(date '+%H') == 09 ]];then
+# elif [[ $(date '+%H') == 08 ]];then
 #     time=$(($RANDOM%10))" 11 * * *"
-# elif [[ $(date '+%H') == 11 ]];then
+# elif [[ $(date '+%H') == 10 ]];then
 #     time=$(($RANDOM%5))" 13 * * *"
-# elif [[ $(date '+%H') == 13 ]];then
+# elif [[ $(date '+%H') == 12 ]];then
 #     time=$(($RANDOM%10))" 0 * * *"
 # else
-#     time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+#     time=$(($RANDOM%10))" 0,2,4,6,8,10,12 * * *"
 # fi
-if [[ ( $(date '+%H') == 00 ) || ( $(date '+%H') == 03 )  || ( $(date '+%H') == 05 )  || ( $(date '+%H') == 07 )  || ( $(date '+%H') == 09 )  || ( $(date '+%H') == 11 )  || ( $(date '+%H') == 13 ) ]] ;then
-    time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+if [[ ( $(date '+%H') == 0 ) || ( $(date '+%H') == 02 )  || ( $(date '+%H') == 04 )  || ( $(date '+%H') == 06 )  || ( $(date '+%H') == 08 )  || ( $(date '+%H') == 10 )  || ( $(date '+%H') == 12 ) ]] ;then
+    time=$(($RANDOM%10))" 0,2,4,6,8,10,12 * * *"
     echo 当前时间为$(date '+%H')时，计划运行时间$time
     sed -i '/cron:/d' .github/workflows/run.yml
     sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
